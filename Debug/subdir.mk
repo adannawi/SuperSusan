8db12f6c28937fe3e6292f03d757526a27460dc1
Clifton Somers <csomers@purdue.edu>
2017-11-20 20:41:59 -0500

    less
    thereis less stuff?

---
+++ Debug/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c 
+
+OBJS += \
+./main.o 
+
+C_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32F4 -DNUCLEO_F410RB -DSTM32F410RBTx -DSTM32 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F410Rx -I"C:/Users/csome/Documents/GitHub/SuperSusan/inc" -I"C:/Users/csome/Documents/GitHub/SuperSusan/CMSIS/core" -I"C:/Users/csome/Documents/GitHub/SuperSusan/CMSIS/device" -I"C:/Users/csome/Documents/GitHub/SuperSusan/HAL_Driver/Inc/Legacy" -I"C:/Users/csome/Documents/GitHub/SuperSusan/HAL_Driver/Inc" -I"C:/Users/csome/Documents/GitHub/SuperSusan/Utilities/STM32F4xx-Nucleo" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
